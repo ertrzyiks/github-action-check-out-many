a7d007d07f17004e10337b5d6e96d665a4596cfc
Mateusz Derks <mateusz.derks@toptal.com>
2023-03-11 13:46:33 +0100

    Add error handloing

--- .github/actions/check-out-many/checkout.sh
+++ .github/actions/check-out-many/checkout.sh
@@ -1,3 +1,5 @@
+die() { echo "$*" 1>&2 ; exit 1; }
+
 while read -r repo;
 do
    if [ ! -z "${repo}" ]; then
@@ -5,6 +7,6 @@ do
     repo=`echo $repo | sed 's/ *$//g'`
 
     echo "Checking out ertrzyiks/$repo";
-    git clone https://github.com/ertrzyiks/$repo.git --depth 1 ${TARGET_FOLDER}/$repo
+    git clone https://github.com/ertrzyiks/$repo.git --depth 1 ${TARGET_FOLDER}/$repo || die "Unable to check out $repo repo"
   fi;
 done
