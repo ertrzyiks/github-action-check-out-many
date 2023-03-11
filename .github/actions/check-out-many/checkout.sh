95990eec576b1892328dba3991368919e6950815
Mateusz Derks <mateusz.derks@toptal.com>
2023-03-11 13:32:04 +0100

    Ignore empty string

--- .github/actions/check-out-many/checkout.sh
+++ .github/actions/check-out-many/checkout.sh
@@ -1,5 +1,7 @@
 while read -r repo;
 do
-   echo "Checking out ertrzyiks/$repo";
-   git clone https://github.com:ertrzyiks/$repo.git --depth 1 ${TARGET_FOLDER}/$repo
+   if [ -z "${repo}" ]; then
+    echo "Checking out ertrzyiks/$repo";
+    git clone https://github.com:ertrzyiks/$repo.git --depth 1 ${TARGET_FOLDER}/$repo
+  fi;
 done
