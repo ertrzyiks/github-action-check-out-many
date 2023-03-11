625da2d1ddd1cf809a0604f207b250e3cbf22c61
Mateusz Derks <mateusz.derks@toptal.com>
2023-03-11 13:35:28 +0100

    Reverse condition

--- .github/actions/check-out-many/checkout.sh
+++ .github/actions/check-out-many/checkout.sh
@@ -1,6 +1,9 @@
 while read -r repo;
 do
-   if [ -z "${repo}" ]; then
+   if [ ! -z "${repo}" ]; then
+    # Trim whitespaces
+    repo=`echo $repo | sed 's/ *$//g'`
+
     echo "Checking out ertrzyiks/$repo";
     git clone https://github.com:ertrzyiks/$repo.git --depth 1 ${TARGET_FOLDER}/$repo
   fi;
